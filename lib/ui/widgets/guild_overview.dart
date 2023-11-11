74fee1861369a66bf430e006668d646b63f2630f
razhterize <arza.mahendra.31@gmail.com>
2023-11-11 19:46:59 +0700

    static overview, anonymous route to details screen

--- lib/ui/widgets/guild_overview.dart
+++ lib/ui/widgets/guild_overview.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pocketbase/pocketbase.dart';
 import 'package:scf_management/models/guild.dart';
-import 'package:scf_management/providers/screen_bloc.dart';
+import 'package:scf_management/providers/guild_bloc.dart';
+import 'package:scf_management/ui/screens/guild_detail_screen.dart';
 import 'package:scf_management/ui/widgets/members_chart.dart';
 
 class GuildOverview extends StatefulWidget {
-  const GuildOverview({super.key, required this.guild});
+  const GuildOverview({super.key, required this.guild, required this.pb});
 
   final Guild guild;
+  final PocketBase pb;
 
   @override
   State<GuildOverview> createState() => _GuildOverviewState();
@@ -24,21 +27,38 @@ class _GuildOverviewState extends State<GuildOverview> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: BlocBuilder<ScreenBloc, ScreenState>(
-        builder: (context, state) {
-          return InkWell(
-            onTap: () {
-              BlocProvider.of<ScreenBloc>(context).add(ShowGuildDetails(guild: guild));
-            },
-            child: guild.members.isNotEmpty ? _membersChart() : _emptyMembers(),
-          );
-        },
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              guild.fullName,
+              style: const TextStyle(fontSize: 15),
+            ),
+          ),
+          Expanded(
+            child: InkWell(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (_) => BlocProvider.value(
+                    value: BlocProvider.of<GuildBloc>(context),
+                    child: GuildDetails(guild: guild, pb: widget.pb),
+                  ),
+                ));
+              },
+              child: guild.members.isNotEmpty ? _membersChart() : _emptyMembers(),
+            ),
+          ),
+        ],
       ),
     );
   }
 
   Widget _membersChart() {
-    return MembersChart(members: guild.members, name: guild.fullName);
+    return Hero(
+      tag: guild.name,
+      child: MembersChart(members: guild.members, name: guild.fullName),
+    );
   }
 
   Widget _emptyMembers() {
