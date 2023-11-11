c876299aff4f597706b17523324e4ee8c95f9636
razhterize <arza.mahendra.31@gmail.com>
2023-11-11 19:45:26 +0700

    final fields

--- lib/models/guild.dart
+++ lib/models/guild.dart
@@ -1,16 +1,20 @@
 import 'package:scf_management/constants/abbreviations.dart';
 import 'package:scf_management/models/member.dart';
+import 'package:equatable/equatable.dart';
 
-class Guild {
+class Guild extends Equatable {
   final String name;
-  late int minScore;
-  late String fullName;
-  List<Member>? _members;
+  final int minScore;
+  late final String fullName;
+  late final List<Member>? _members;
 
-  Guild({this.name = "", List<Member>? members}) {
+  Guild({this.name = "", List<Member>? members, this.minScore = 0}) {
     fullName = guildNames[name] ?? "";
     _members = members;
   }
 
   List<Member> get members => _members ?? [];
+
+  @override
+  List<Object?> get props => [name, members];
 }
