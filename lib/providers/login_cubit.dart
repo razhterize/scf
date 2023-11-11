5f5e4e2a798391062396d9ef604c587bfecee5a5
razhterize <arza.mahendra.31@gmail.com>
2023-11-11 23:51:24 +0700

    make pocketbase public fix stuck oauth loading

--- lib/providers/login_cubit.dart
+++ lib/providers/login_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pocketbase/pocketbase.dart';
 import 'package:scf_management/constants/enums.dart';
@@ -25,52 +26,48 @@ class LoginState extends Equatable {
 }
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit({required PocketBase pb}) : super(LoginState(loginStatus: LoginStatus.unknown, pb: pb)) {
-    _pb = pb;
-    if (_pb.authStore.isValid) {
-      authRefresh(pb);
+  LoginCubit({required this.pb}) : super(LoginState(loginStatus: LoginStatus.unknown, pb: pb)) {
+    if (pb.authStore.isValid) {
+      authRefresh();
       return;
     } else {
       emit(state.copyWith(loginStatus: LoginStatus.failed));
     }
   }
 
-  Future<void> authRefresh(PocketBase pb) async {
+  Future<void> authRefresh() async {
     var model = await pb.collection("discord_auth").authRefresh();
     emit(state.copyWith(authModel: model, loginStatus: LoginStatus.success));
   }
 
   void logout() {
-    _pb.authStore.clear();
+    pb.authStore.clear();
     emit(state.copyWith(loginStatus: LoginStatus.failed));
   }
 
   void loginWithDiscord() async {
     emit(state.copyWith(loginStatus: LoginStatus.processing));
-    if (_pb.authStore.isValid) {
-      var model = await _pb.collection("discord_auth").authRefresh();
-      emit(state.copyWith(loginStatus: LoginStatus.success, pb: _pb, authModel: model));
+    if (pb.authStore.isValid) {
+      var model = await pb.collection("discord_auth").authRefresh();
+      emit(state.copyWith(loginStatus: LoginStatus.success, pb: pb, authModel: model));
       return;
     }
     try {
-      _pb.authStore.onChange.listen((event) async {
-        if (_pb.authStore.isValid) {
-          emit(state.copyWith(loginStatus: LoginStatus.success, pb: _pb));
-          return;
-        }
-      });
-      await _pb.collection("discord_auth").authWithOAuth2(
+      await pb.collection("discord_auth").authWithOAuth2(
         "discord",
         (url) async {
           await launchUrl(url);
         },
         scopes: ['identify', 'guilds'],
-      );
-      emit(state.copyWith(loginStatus: LoginStatus.processing));
+      ).then((value) {
+        if (pb.authStore.isValid) {
+          authRefresh();
+        }
+      });
     } catch (e) {
       emit(state.copyWith(loginStatus: LoginStatus.failed));
     }
   }
 
-  late final PocketBase _pb;
+  final PocketBase pb;
 }
