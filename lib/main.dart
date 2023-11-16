3ec3d4c05ef8d6fd2913719acf90e46ceb43b2b3
razhterize <arza.mahendra.31@gmail.com>
2023-11-16 12:22:06 +0700

    windowManager to change windows window title

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:window_manager/window_manager.dart';
 import 'app.dart';
 
 void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+  windowManager.waitUntilReadyToShow().then((_) async {
+    await windowManager.setTitle("PGR Guild Management");
+  });
   await dotenv.load(fileName: ".env");
   var prefs = await SharedPreferences.getInstance();
   runApp(SCFManagement(sharedPreferences: prefs));
