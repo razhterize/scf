b223163027401e7f034585808300bc37cebf7fba
razhterize <arza.mahendra.31@gmail.com>
2023-11-17 14:13:34 +0700

    added web platform check

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -5,11 +6,13 @@ import 'package:window_manager/window_manager.dart';
 import 'app.dart';
 
 void main(List<String> args) async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await windowManager.ensureInitialized();
-  windowManager.waitUntilReadyToShow().then((_) async {
-    await windowManager.setTitle("PGR Guild Management");
-  });
+  if (!kIsWeb) {
+    WidgetsFlutterBinding.ensureInitialized();
+    await windowManager.ensureInitialized();
+    windowManager.waitUntilReadyToShow().then((_) async {
+      await windowManager.setTitle("PGR Guild Management");
+    });
+  }
   await dotenv.load(fileName: ".env");
   var prefs = await SharedPreferences.getInstance();
   runApp(SCFManagement(sharedPreferences: prefs));
