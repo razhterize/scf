c92caa8d2e22997f67def942bf00ab3725341a40
razhterize <arza.mahendra.31@gmail.com>
2023-11-20 15:30:01 +0700

    idk why fetch_client failed to build on windows

--- lib/app.dart
+++ lib/app.dart
@@ -6,8 +6,8 @@ import 'package:scf_management/providers/login_cubit.dart';
 import 'package:scf_management/providers/settings_bloc.dart';
 import 'package:scf_management/ui/screens/home_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:fetch_client/fetch_client.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
+// import 'package:fetch_client/fetch_client.dart';
+// import 'package:flutter/foundation.dart' show kIsWeb;
 
 class SCFManagement extends StatefulWidget {
   const SCFManagement({super.key, required this.sharedPreferences});
@@ -29,14 +29,14 @@ class _SCFManagementState extends State<SCFManagement> {
     if (dbUrl == "" || dbUrl == null) {
       dbUrl = dotenv.get("PB_URL");
     }
-    pb =  PocketBase(
+    pb = PocketBase(
       dbUrl,
       authStore: AsyncAuthStore(
         save: (String data) => sharedPreferences.setString('pb_auth', data),
         initial: sharedPreferences.getString('pb_auth'),
         clear: () => sharedPreferences.setString("pb_auth", ""),
       ),
-      httpClientFactory: kIsWeb ? () => FetchClient(mode: RequestMode.cors) : null
+      // httpClientFactory: kIsWeb ? () => FetchClient(mode: RequestMode.cors) : null,
     );
     super.initState();
   }
@@ -56,6 +56,7 @@ class _SCFManagementState extends State<SCFManagement> {
         builder: (context, state) {
           BlocProvider.of<SettingBloc>(context).add(GetSettings());
           return MaterialApp(
+            // title: kIsWeb ? "SCF Management" : "",
             debugShowCheckedModeBanner: false,
             theme: state.lightMode ? ThemeData.light() : ThemeData.dark(),
             home: HomeScreen(pb: pb),
