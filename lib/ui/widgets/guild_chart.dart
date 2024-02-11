770038fb90c0449759f8f538e0c0d43125f065db
razhterize <arza.mahendra.31@gmail.com>
2024-02-11 16:20:11 +0700

    damned chart didnt update immediately so I changed it to depends on GuildBloc. should've done that first time around

--- lib/ui/widgets/guild_chart.dart
+++ lib/ui/widgets/guild_chart.dart
@@ -1,25 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:scf_management/blocs/guild_bloc.dart';
 import 'package:scf_management/constants/enums.dart';
 import 'package:scf_management/constants/theme.dart';
 import 'package:scf_management/models/member.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 class GuildClearChart extends StatefulWidget {
-  const GuildClearChart({super.key, required this.members, required this.name});
+  const GuildClearChart({super.key});
 
-  final List<Member> members;
-  final String name;
+  // final List<Member> members;
+  // final String name;
 
   @override
   State<GuildClearChart> createState() => _GuildClearChartState();
 }
 
 class _GuildClearChartState extends State<GuildClearChart> {
-  late final List<Member> members;
+  // late final List<Member> members;
 
   @override
   void initState() {
-    members = widget.members;
+    // members = widget.members;
     super.initState();
   }
 
@@ -27,22 +29,26 @@ class _GuildClearChartState extends State<GuildClearChart> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(2.0),
-      child: SfCircularChart(
-        legend: const Legend(
-          isVisible: true,
-          position: LegendPosition.bottom,
-          overflowMode: LegendItemOverflowMode.wrap,
-        ),
-        series: <CircularSeries>[
-          PieSeries<GuildChartData, String>(
-            animationDuration: 1,
-            dataSource: _chartData(members),
-            pointColorMapper: (GuildChartData data, _) => data.color,
-            xValueMapper: (GuildChartData data, _) => data.status,
-            yValueMapper: (GuildChartData data, _) => data.count,
-            name: widget.name,
-          ),
-        ],
+      child: BlocBuilder<GuildBloc, GuildState>(
+        builder: (context, state) {
+          return SfCircularChart(
+            legend: const Legend(
+              isVisible: true,
+              position: LegendPosition.bottom,
+              overflowMode: LegendItemOverflowMode.wrap,
+            ),
+            series: <CircularSeries>[
+              PieSeries<GuildChartData, String>(
+                animationDuration: 1,
+                dataSource: _chartData(state.guild.members),
+                pointColorMapper: (GuildChartData data, _) => data.color,
+                xValueMapper: (GuildChartData data, _) => data.status,
+                yValueMapper: (GuildChartData data, _) => data.count,
+                name: state.guild.fullName,
+              ),
+            ],
+          );
+        },
       ),
     );
   }
