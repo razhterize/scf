317b5c078942470d82eca427a073bbf6d6e4e8af
razhterize <arza.mahendra.31@gmail.com>
2024-02-11 16:14:17 +0700

    added logger

---
+++ lib/logger.dart
@@ -0,0 +1,32 @@
+import 'package:logger/logger.dart';
+
+const bool _debug = true;
+
+final Logger logger = Logger(printer: Printer(), filter: Filter(),);
+
+void info(String message) => logger.i(message);
+void warning(String message) => logger.w(message);
+void fatal(String message) => logger.f(message);
+void error(String message) => logger.e(message);
+void debug(String message) => logger.d(message);
+void trace(String message) => logger.t(message);
+
+class Printer extends LogPrinter {
+  @override
+  List<String> log(LogEvent event) {
+    return ["[${event.time.toLocal()} | ${event.level.name.toUpperCase()}]", "${event.message}",""];
+  }
+}
+
+class Filter extends LogFilter {
+  final List<Level> levels = [Level.info, Level.warning, Level.error, Level.fatal];
+  @override
+  bool shouldLog(LogEvent event) {
+    if (_debug) {
+      return true;
+    } else if (levels.contains(event.level)) {
+      return true;
+    }
+    return false;
+  }
+}
