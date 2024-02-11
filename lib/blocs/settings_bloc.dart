ba430105c8fdd12569421f0af6e856099210df59
razhterize <arza.mahendra.31@gmail.com>
2024-02-11 16:18:18 +0700

    eh shit I forgot this one from previous commit

---
+++ lib/blocs/settings_bloc.dart
@@ -0,0 +1,55 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SettingBloc extends Bloc<SettingEvent, SettingState> {
+  SettingBloc({required this.sharedPreferences}) : super(const SettingState()) {
+    on<GetSettings>(fetchSettings);
+    on<SetSettings>(setSettings);
+  }
+
+  void fetchSettings(GetSettings event, Emitter<SettingState> emit) {
+    bool lightMode =  sharedPreferences.getBool('lightMode') ?? false;
+    String databaseUrl = sharedPreferences.getString('databaseUrl') ?? dotenv.get("PB_URL");
+    return emit(state.copyWith(lightMode: lightMode, databaseUrl: databaseUrl));
+  }
+
+  void setSettings(SetSettings event, Emitter<SettingState> emit) {
+    sharedPreferences.setBool("lightMode", event.lightMode);
+    sharedPreferences.setString("databaseUrl", event.databaseUrl);
+    return emit(state.copyWith(lightMode: event.lightMode, databaseUrl: event.databaseUrl));
+  }
+
+  final SharedPreferences sharedPreferences;
+}
+
+final class SettingState extends Equatable {
+  const SettingState({this.lightMode = false, this.databaseUrl = ""});
+
+  final bool lightMode;
+  final String databaseUrl;
+
+  SettingState copyWith({bool? lightMode, String? databaseUrl}) {
+    return SettingState(
+      lightMode: lightMode ?? this.lightMode,
+      databaseUrl: databaseUrl ?? this.databaseUrl,
+    );
+  }
+
+  @override
+  List<Object> get props => [lightMode, databaseUrl];
+}
+
+sealed class SettingEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+final class GetSettings extends SettingEvent {}
+
+final class SetSettings extends SettingEvent {
+  SetSettings({this.lightMode = false, this.databaseUrl = ""});
+  final bool lightMode;
+  final String databaseUrl;
+}
