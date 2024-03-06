be7eb0aa4215e4c3cb3fb0429f4485c3c927f2da
razhterize <arza.mahendra.31@gmail.com>
2024-03-06 13:32:04 +0700

    replace double quotes with single quotes cuz why not

--- lib/configs.dart
+++ lib/configs.dart
@@ -2,4 +2,4 @@ String get databaseUrl => "https://scf-management.pockethost.io/";
 
 String get version => '1.1';
 
-Map<String, String> get adminLogin => {'username': "<username>", "password": "<password>"};
+Map<String, String> get adminLogin => {'username': '<username>', 'password': '<password>'};
