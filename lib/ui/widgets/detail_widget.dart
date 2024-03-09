5cda79f6d4176bdfec7b2e6d80b120940b16851a
razhterize <arza.mahendra.31@gmail.com>
2024-03-10 01:45:24 +0700

    added guild switcher on AppBar icon added initial SwitchState if it doesnt contain valid keys

--- lib/ui/widgets/detail_widget.dart
+++ lib/ui/widgets/detail_widget.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:loading_animation_widget/loading_animation_widget.dart';
 
-import '../../blocs/guild_bloc.dart';
+import '../../constants.dart';
 import '../../blocs/login_bloc.dart';
 import '../../blocs/switch_cubit.dart';
-
 import 'views.dart';
+
+import 'package:logging/logging.dart';
 
 class DetailWidget extends StatefulWidget {
   const DetailWidget({super.key});
@@ -16,41 +16,51 @@ class DetailWidget extends StatefulWidget {
 }
 
 class _DetailWidgetState extends State<DetailWidget> {
+  final logger = Logger("Detail");
+
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => GuildBloc(
-        context.read<LoginBloc>().pb,
-        context.read<SwitchCubit>().state.name,
-      ),
-      child: Scaffold(
-        appBar: _appBar(),
-        body: Stack(
-          children: [
-            Center(
-              child: BlocBuilder<GuildBloc, GuildState>(
-                builder: (context, state) => state.busy ? _loading() : Container(),
-              ),
-            ),
-            BlocBuilder<SwitchCubit, SwitchState>(
-              builder: (context, state) => const Views(),
-            ),
-          ],
-        ),
-      ),
+    if (!guildNames.keys.toList().contains(context.read<SwitchCubit>().state.name)) {
+      context.read<SwitchCubit>().switchGuild(context.read<LoginBloc>().state.authStore.model.data['managed_guilds'].first);
+    }
+    return Scaffold(
+      appBar: _appBar(),
+      body: const Views(),
     );
   }
 
   AppBar _appBar() {
     return AppBar(
-      title: BlocBuilder<GuildBloc, GuildState>(builder: (context, state) => Text(state.guild.fullName ?? state.guild.name)),
+      title: BlocBuilder<SwitchCubit, SwitchState>(builder: (context, state) => Text(guildNames[state.name] ?? state.name)),
       centerTitle: true,
       leading: IconButton(
         icon: const Icon(Icons.person),
-        onPressed: () {},
+        onPressed: () {
+          showMenu(
+            context: context,
+            position: const RelativeRect.fromLTRB(50, 0, 100, 100),
+            items: [
+              for (var guild in context.read<LoginBloc>().state.authStore.model.data['managed_guilds'])
+                PopupMenuItem(
+                  child: MaterialButton(
+                    child: Row(
+                      children: [const Icon(Icons.group), const Text('   '), Text(guildNames[guild] ?? guild)],
+                    ),
+                    onPressed: () => context.read<SwitchCubit>().switchGuild(guild),
+                  ),
+                ),
+              PopupMenuItem(
+                child: MaterialButton(
+                  child: const Row(
+                    children: [Icon(Icons.logout), Text("Logout")],
+                  ),
+                  onPressed: () => context.read<LoginBloc>().add(Logout()),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
-
-  Widget _loading() => LoadingAnimationWidget.hexagonDots(color: Colors.cyanAccent, size: 40);
 }
