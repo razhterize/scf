699221d7fb6f3573e15d39358a8592ad819a93f2
razhterize <arza.mahendra.31@gmail.com>
2024-03-13 21:56:25 +0700

    fixed BlocProvider doesnt passed to EditMemberWidget chagne imports to relativeimports

--- lib/ui/widgets/member_details.dart
+++ lib/ui/widgets/member_details.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:scf_new/ui/widgets/member_edit_widgets.dart';
-import 'package:scf_new/ui/widgets/status_selection.dart';
 
+import '../../blocs/guild_bloc.dart';
+import '../../ui/widgets/member_edit_widgets.dart';
+import '../../ui/widgets/status_selection.dart';
 import '../../blocs/switch_cubit.dart';
 import '../../enums.dart';
 import '../../models/member_model.dart';
@@ -57,8 +58,14 @@ class _MemberDetailState extends State<MemberDetail> {
   void openEditWindow(Member member) {
     showModalBottomSheet(
       context: context,
-      builder: (context) {
-        return EditMemberWidget(member: member);
+      builder: (_) {
+        logger.fine("Current Guild Bloc State: ${context.read<GuildBloc>().state.guild.name}");
+        return BlocProvider.value(
+          value: context.read<GuildBloc>(),
+          child: EditMemberWidget(
+            member: member,
+          ),
+        );
       },
     );
   }
