f77a18e8cff23ef91221434e9903bdbc37df6675
razhterize <arza.mahendra.31@gmail.com>
2024-03-16 01:58:31 +0700

    bump version

--- lib/configs.dart
+++ lib/configs.dart
@@ -1,7 +1,7 @@
 
 String get databaseUrl => "https://scf-management.pockethost.io";
 
-String get version => '1.1';
+String get version => '1.1.0+3';
 
 Map<String, String> get adminLogin => {'username': '<username>', 'password': '<password>'};
 
