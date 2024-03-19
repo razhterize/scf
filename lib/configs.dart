81c7ff25484448f550bba01635c36acc7b37ba2e
razhterize <arza.mahendra.31@gmail.com>
2024-03-19 23:26:02 +0700

    deploy: update version 1.1.1+1

--- lib/configs.dart
+++ lib/configs.dart
@@ -4,7 +4,7 @@ set databaseUrl(String url) => _dbUrl = url;
 
 String get databaseUrl => _dbUrl;
 
-String get version => '1.1.1+0';
+String get version => '1.1.1+1';
 
 Map<String, String> get adminLogin => {
       'username': '<username>',
