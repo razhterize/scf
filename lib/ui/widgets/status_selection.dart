00291da30b9d3a3fd1f6de16d53b026107378078
razhterize <arza.mahendra.31@gmail.com>
2024-03-21 11:07:38 +0700

    feat: added energy damage and extra button for hidden Node

--- lib/ui/widgets/status_selection.dart
+++ lib/ui/widgets/status_selection.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:scf_new/blocs/switch_cubit.dart';
 
 import '../../constants.dart';
 import '../../enums.dart';
@@ -22,6 +23,39 @@ class StatusSelections extends StatelessWidget {
     final selectionCubit = context.read<SelectionCubit>();
     return Wrap(
       children: [
+        BlocBuilder<SwitchCubit, SwitchState>(
+          builder: (context, state) => state.mode == ManagementMode.maze
+              ? SizedBox(
+                  width: 250,
+                  child: energyDamageField(context),
+                )
+              : Container(),
+        ),
+        BlocBuilder<SwitchCubit, SwitchState>(
+          builder: (context, state) => state.mode == ManagementMode.maze
+              ? Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 2, 16, 2),
+                  child: ChoiceChip(
+                    label: const Text("Hidden"),
+                    selected: member.mazeData.hidden ?? false,
+                    onSelected: (value) {
+                      if (selectionCubit.state.isNotEmpty) {
+                        showDialog(
+                          context: context,
+                          builder: (_) => Popup(
+                            "You're about to change ${selectionCubit.state.length} members hidden status\nDo it?",
+                            callback: () => _batchHiddenChange(context, value),
+                          ),
+                        );
+                        return;
+                      }
+                      member.mazeData.hidden = value;
+                      context.read<GuildCubit>().updateMember(member);
+                    },
+                  ),
+                )
+              : Container(),
+        ),
         for (var status in statuses)
           Padding(
             padding: const EdgeInsets.all(2.0),
@@ -30,14 +64,14 @@ class StatusSelections extends StatelessWidget {
               label: Text(
                 statusNames[status] ?? status.name,
                 style: TextStyle(
-                  color: member.mazeStatus == status ||
+                  color: member.mazeData.status == status ||
                           member.siegeStatus == status
                       ? Colors.black
                       : Colors.white,
                 ),
               ),
-              selected:
-                  member.siegeStatus == status || member.mazeStatus == status,
+              selected: member.siegeStatus == status ||
+                  member.mazeData.status == status,
               onSelected: (value) {
                 if (selectionCubit.state.isNotEmpty) {
                   showDialog(
@@ -51,8 +85,7 @@ class StatusSelections extends StatelessWidget {
                 }
                 SiegeStatus.values.contains(status)
                     ? member.siegeStatus = status
-                    : member.mazeStatus = status;
-
+                    : member.mazeData.status = status;
                 context.read<GuildCubit>().updateMember(member);
               },
             ),
@@ -61,13 +94,48 @@ class StatusSelections extends StatelessWidget {
     );
   }
 
+  Form energyDamageField(BuildContext context) {
+    final formKey = GlobalKey<FormState>();
+    final controller = TextEditingController();
+    controller.text = member.mazeData.energyDamage.toString();
+    return Form(
+      key: formKey,
+      child: TextFormField(
+        controller: controller,
+        decoration: const InputDecoration(
+            label: Text("Energy Damage"),
+            enabledBorder: InputBorder.none,
+            isDense: true),
+        keyboardType: TextInputType.number,
+        validator: (value) => RegExp(r'^[0-9]+$').hasMatch(value!)
+            ? null
+            : 'Only numbers allowed',
+        onFieldSubmitted: (value) {
+          if (formKey.currentState!.validate()) {
+            member.mazeData.energyDamage = int.parse(value);
+            context.read<GuildCubit>().updateMember(member);
+          }
+        },
+      ),
+    );
+  }
+
   void _batchStatusChange(BuildContext context, status) {
     print("Batch Status called");
     context.read<SelectionCubit>().doSomethingAboutSelectedMembers((member) {
       SiegeStatus.values.contains(status)
           ? member.siegeStatus = status
-          : member.mazeStatus = status;
+          : member.mazeData.status = status;
       context.read<GuildCubit>().updateMember(member);
     });
+  }
+
+  void _batchHiddenChange(BuildContext context, bool value) {
+    context.read<SelectionCubit>().doSomethingAboutSelectedMembers(
+      (member) {
+        member.mazeData.hidden = value;
+        context.read<GuildCubit>().updateMember(member);
+      },
+    );
   }
 }
