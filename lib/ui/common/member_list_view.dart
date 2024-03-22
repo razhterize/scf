3835e197ec47391b9530054352896e040f294ffe
razhterize <arza.mahendra.31@gmail.com>
2024-03-23 02:35:55 +0700

    feat: listview for member_info TODO: animate children in order to make some kind of stair effect

---
+++ lib/ui/common/member_list_view.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:scf_new/blocs/guild_cubit.dart';
+import 'package:scf_new/ui/common/animations/change_screen.dart';
+import 'package:scf_new/ui/common/loading.dart';
+import 'package:scf_new/ui/common/member_info.dart';
+
+class MemberListView extends StatefulWidget {
+  const MemberListView({super.key});
+
+  @override
+  State<MemberListView> createState() => _MemberListViewState();
+}
+
+class _MemberListViewState extends State<MemberListView> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Center(
+          child: BlocBuilder<GuildCubit, GuildState>(
+            builder: (context, state) =>
+                state.busy ? const LoadingIndicator() : const SizedBox(),
+          ),
+        ),
+        BlocBuilder<GuildCubit, GuildState>(
+          builder: (context, state) {
+            return ListView(
+              children: state.guild.members
+                  .map(
+                    (member) => MemberInfo(member),
+                  )
+                  .toList(),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
