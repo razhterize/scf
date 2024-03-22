f3b148893fcbf4bb612f5971769ae62e5babbc8e
razhterize <arza.mahendra.31@gmail.com>
2024-03-23 02:30:34 +0700

    feat: base layout builder for different screens (WIP)

---
+++ lib/ui/common/layout.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Layout extends StatefulWidget {
+  const Layout({super.key});
+
+  @override
+  State<Layout> createState() => _LayoutState();
+}
+
+class _LayoutState extends State<Layout> {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < 600) {
+          return const MobileLayout();
+        } else {
+          return const DesktopLayout();
+        }
+      },
+    );
+  }
+}
+
+class MobileLayout extends StatelessWidget {
+  const MobileLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class DesktopLayout extends StatelessWidget {
+  const DesktopLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
