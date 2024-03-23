41f237784e78b69d3b73c54ecb75febbd7a7d442
razhterize <arzaditya@outlook.com>
2024-03-23 10:48:29 +0700

    feat: animation in StatusSelection itself

--- lib/ui/common/member_status_selection.dart
+++ lib/ui/common/member_status_selection.dart
@@ -6,6 +6,7 @@ import 'package:scf_new/constants.dart';
 import 'package:scf_new/enums.dart';
 
 import '../../models/member_model.dart';
+import 'animations/sliding_fade_transition.dart';
 
 class MemberStatusSelection extends StatelessWidget {
   const MemberStatusSelection(this.member, {super.key});
@@ -14,45 +15,42 @@ class MemberStatusSelection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<SwitchCubit, SwitchState>(
-      builder: (context, state) {
-        switch (state.mode) {
-          case ManagementMode.siege:
-            return DropdownButton<SiegeStatus>(
-              value: member.siegeStatus,
-              items: [
-                for (var s in SiegeStatus.values)
-                  DropdownMenuItem(
-                    value: s,
-                    child: Text(statusNames[s] ?? s.name),
-                  )
-              ],
-              onChanged: (value) {
-                member.siegeStatus = value!;
-                context.read<GuildCubit>().updateMember(member);
-              },
-            );
-          case ManagementMode.maze:
-            return DropdownButton<MazeStatus>(
-              value: member.mazeData.status,
-              items: [
-                for (var m in MazeStatus.values)
-                  DropdownMenuItem(
-                    value: m,
-                    child: Text(statusNames[m] ?? m.name),
-                  )
-              ],
-              onChanged: (value) {
-                member.mazeData.status = value!;
-                context.read<GuildCubit>().updateMember(member);
-              },
-            );
-          default:
-            return IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.edit),
-            );
-        }
+    final mode = context.watch<SwitchCubit>().state.mode;
+    return SlidingFadeTransition(
+      duration: const Duration(milliseconds: 500),
+      child: switch (mode) {
+        ManagementMode.siege => DropdownButton<SiegeStatus>(
+            value: member.siegeStatus,
+            items: [
+              for (var s in SiegeStatus.values)
+                DropdownMenuItem(
+                  value: s,
+                  child: Text(statusNames[s] ?? s.name),
+                )
+            ],
+            onChanged: (value) {
+              member.siegeStatus = value!;
+              context.read<GuildCubit>().updateMember(member);
+            },
+          ),
+        ManagementMode.maze => DropdownButton<MazeStatus>(
+            value: member.mazeData.status,
+            items: [
+              for (var m in MazeStatus.values)
+                DropdownMenuItem(
+                  value: m,
+                  child: Text(statusNames[m] ?? m.name),
+                )
+            ],
+            onChanged: (value) {
+              member.mazeData.status = value!;
+              context.read<GuildCubit>().updateMember(member);
+            },
+          ),
+        _ => IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.edit),
+          ),
       },
     );
   }
