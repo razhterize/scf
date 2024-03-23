bf06a92214877fd5d85833579c52eb269771a51b
razhterize <arzaditya@outlook.com>
2024-03-23 19:59:47 +0700

    feat: theming

--- lib/ui/themes.dart
+++ lib/ui/themes.dart
@@ -2,11 +2,24 @@ import 'package:flutter/material.dart';
 
 final darkTheme = ThemeData(
   useMaterial3: true,
-  colorScheme: ColorScheme.fromSeed(
-    seedColor: Colors.orangeAccent,
+  colorScheme: ColorScheme(
     brightness: Brightness.dark,
+    primary: const Color.fromARGB(255, 68, 41, 0),
+    onPrimary: Colors.white,
+    secondary: Colors.orangeAccent,
+    onSecondary: Colors.white,
+    error: Colors.red,
+    onError: Colors.white,
+    background: Color.fromARGB(255, 18, 9, 0),
+    onBackground: Colors.white,
+    surface: Colors.orangeAccent.shade700,
+    onSurface: Colors.white,
+    outline: Colors.white,
   ),
+  textTheme: const TextTheme(),
 );
+
+
 
 ThemeData customTheme({Brightness? brightness, MaterialAccentColor? seed}) {
   return ThemeData(
