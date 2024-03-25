b06f6360d97c3e8b30834129e350bf0ddb571016
razhterize <arzaditya@outlook.com>
2024-03-25 20:05:26 +0700

    feat: added maxLines on ListTile title

--- lib/ui/widgets/member_info.dart
+++ lib/ui/widgets/member_info.dart
@@ -18,7 +18,10 @@ class MemberInfo extends StatelessWidget {
       builder: (context, state) {
         return ListTile(
           selected: context.read<SelectionCubit>().isSelected(member),
-          title: Text(member.name),
+          title: Text(
+            member.name,
+            maxLines: 2,
+          ),
           selectedColor: Theme.of(context).textSelectionTheme.cursorColor,
           subtitle: Text(member.pgrId.toString()),
           onTap: () => context.read<SelectionCubit>().changeSelect(member),
