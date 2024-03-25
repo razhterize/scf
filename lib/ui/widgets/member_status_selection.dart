531304b18b9d32ce2a61f6e3e09586a25594a581
razhterize <arzaditya@outlook.com>
2024-03-25 20:05:53 +0700

    refractor: moved _statusSelection to method

--- lib/ui/widgets/member_status_selection.dart
+++ lib/ui/widgets/member_status_selection.dart
@@ -16,56 +16,59 @@ class MemberStatusSelection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final mode = context.watch<SwitchCubit>().state.mode;
     return SlidingFadeTransition(
-      duration: const Duration(milliseconds: 500),
-      child: switch (mode) {
-        ManagementMode.siege => DropdownButton<SiegeStatus>(
-            value: member.siegeStatus,
-            items: [
-              for (var s in SiegeStatus.values)
-                DropdownMenuItem(
-                  value: s,
-                  child: Text(
-                    statusNames[s] ?? s.name,
-                    style: textStyle(s),
-                  ),
-                )
-            ],
-            onChanged: (value) {
-              member.siegeStatus = value!;
-              context.read<GuildCubit>().updateMember(member);
-            },
-          ),
-        ManagementMode.maze => DropdownButton<MazeStatus>(
-            value: member.mazeData.status,
-            items: [
-              for (var m in MazeStatus.values)
-                DropdownMenuItem(
-                  value: m,
-                  child: Text(
-                    statusNames[m] ?? m.name,
-                    style: textStyle(m),
-                  ),
-                )
-            ],
-            onChanged: (value) {
-              member.mazeData.status = value!;
-              context.read<GuildCubit>().updateMember(member);
-            },
-          ),
-        _ => IconButton(
-            onPressed: () {
-              showModalBottomSheet(
-                  context: context,
-                  builder: (_) {
-                    return MemberEdit(member: member);
-                  });
-            },
-            icon: const Icon(Icons.edit),
-          ),
-      },
-    );
+        duration: const Duration(milliseconds: 500),
+        child: _statusSelection(context));
+  }
+
+  Widget _statusSelection(BuildContext context) {
+    final mode = context.watch<SwitchCubit>().state.mode;
+    return switch (mode) {
+      ManagementMode.siege => DropdownButton<SiegeStatus>(
+          value: member.siegeStatus,
+          items: [
+            for (var s in SiegeStatus.values)
+              DropdownMenuItem(
+                value: s,
+                child: Text(
+                  statusNames[s] ?? s.name,
+                  style: textStyle(s),
+                ),
+              )
+          ],
+          onChanged: (value) {
+            member.siegeStatus = value!;
+            context.read<GuildCubit>().updateMember(member);
+          },
+        ),
+      ManagementMode.maze => DropdownButton<MazeStatus>(
+          value: member.mazeData.status,
+          items: [
+            for (var m in MazeStatus.values)
+              DropdownMenuItem(
+                value: m,
+                child: Text(
+                  statusNames[m] ?? m.name,
+                  style: textStyle(m),
+                ),
+              )
+          ],
+          onChanged: (value) {
+            member.mazeData.status = value!;
+            context.read<GuildCubit>().updateMember(member);
+          },
+        ),
+      _ => IconButton(
+          onPressed: () {
+            showModalBottomSheet(
+                context: context,
+                builder: (_) {
+                  return MemberEdit(member: member);
+                });
+          },
+          icon: const Icon(Icons.edit),
+        ),
+    };
   }
 
   TextStyle textStyle(dynamic status) => TextStyle(color: statusColors[status]);
