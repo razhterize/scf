3bbf1521664cdeadf10f6cec8986d156c8d187ab
razhterize <arzaditya@outlook.com>
2024-04-07 06:48:02 +0700

    build: bump version

--- lib/configs.dart
+++ lib/configs.dart
@@ -4,7 +4,7 @@ set databaseUrl(String url) => _dbUrl = url;
 
 String get databaseUrl => _dbUrl;
 
-String get version => '1.2.0+2';
+String get version => '1.2.0+4';
 
 Map<String, String> get adminLogin => {
       'username': '<username>',
