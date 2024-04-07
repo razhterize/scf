bae92d604809af555354a42f029655c4b15cd962
razhterize <arzaditya@outlook.com>
2024-04-07 11:13:42 +0700

    feat: added guild field

--- lib/models/member_model.dart
+++ lib/models/member_model.dart
@@ -5,19 +5,29 @@ class Member {
   String id;
   String name;
   int pgrId;
+  List guild;
   String? discordId;
   String? discordUsername;
   SiegeStatus siegeStatus;
   MazeData mazeData;
 
-  Member(this.id, this.name, this.pgrId, this.siegeStatus, this.mazeData,
-      {this.discordId, this.discordUsername});
+  Member(
+    this.id,
+    this.name,
+    this.pgrId,
+    this.guild,
+    this.siegeStatus,
+    this.mazeData, {
+    this.discordId,
+    this.discordUsername,
+  });
 
   factory Member.fromRecord(RecordModel record) {
     return Member(
       record.id,
       record.getStringValue('name'),
       record.getIntValue('pgr_id'),
+      record.getListValue('guild'),
       SiegeStatus.values.byName(record.data['siege']['status']),
       MazeData.fromMap(record.data['maze']),
       discordId: record.getStringValue('discord_id'),
@@ -25,19 +35,23 @@ class Member {
     );
   }
 
-  Map<String, dynamic> toMap(
-          {String? name,
-          int? pgrId,
-          String? discordId,
-          String? discordUsername}) =>
-      {
-        'name': name ?? this.name,
-        'pgr_id': pgrId ?? this.pgrId,
-        'siege': {'status': siegeStatus.name},
-        'maze': mazeData.toMap(),
-        'discord_id': discordId ?? this.discordId,
-        'discord_username': discordUsername ?? this.discordUsername
-      };
+  Map<String, dynamic> toMap({
+    String? name,
+    int? pgrId,
+    String? discordId,
+    List? guild,
+    String? discordUsername,
+  }) {
+    return {
+      'name': name ?? this.name,
+      'pgr_id': pgrId ?? this.pgrId,
+      'guild': guild ?? this.guild,
+      'siege': {'status': siegeStatus.name},
+      'maze': mazeData.toMap(),
+      'discord_id': discordId ?? this.discordId,
+      'discord_username': discordUsername ?? this.discordUsername
+    };
+  }
 }
 
 class MazeData {
